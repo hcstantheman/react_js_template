ebad22aca5ffa94bf1e19d9af024b3f2db87d727
hcstantheman <62427735+hcstantheman@users.noreply.github.com>
2024-01-19 16:04:35 +0900

    first commit

---
+++ pages/hello.js
@@ -0,0 +1,9 @@
+import styles from './css/hello.module.scss'
+
+export default function Hello() {
+  return (
+    <div className={styles.container}>
+      <p>hello this is stanley</p>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from './css/index.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <p>hi</p>
+    </div>
+  )
+}
